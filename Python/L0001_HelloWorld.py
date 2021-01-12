5a9b64f2298f81d58fd477cbbf4d54c70793688f
Smita <smitabusar@gmail.com>
2021-01-12 15:54:46 -0800

    Run Hello World Python

---
+++ Python/L0001_HelloWorld.py
@@ -0,0 +1,4 @@
+# Prerequisite : Python is installed version 3.8 or greater
+# To run specify on windows : py L0001_HelloWorld.py  
+# To run specify on MacOS : python3 L0001_HelloWorld.py
+print ("Hello World !!")
