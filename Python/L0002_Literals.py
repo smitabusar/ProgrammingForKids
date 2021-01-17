26d50fc4f21bf20a0f36e784f394aa628403473e
Smita <smitabusar@gmail.com>
2021-01-17 12:19:52 -0800

    added literals

---
+++ Python/L0002_Literals.py
@@ -0,0 +1,12 @@
+# Strings are characters enclosed in double or single quotes
+print ("Any characters enclosed in double or single quotes is considered string in python")
+print ('I am also a string')
+
+#Print integers or float
+print (5)
+print (-5)
+print (5.5)
+
+#Print booleans which have the value True or False
+print(True)
+print(False)
