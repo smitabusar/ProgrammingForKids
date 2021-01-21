44d9528e5a2e69169cd70f40538efcb9993a5b09
Smita <smitabusar@gmail.com>
2021-01-20 18:00:12 -0800

    Print and Aritmetic Operations

--- Python/L0001_HelloWorld.py
+++ Python/L0001_HelloWorld.py
@@ -2,3 +2,4 @@
 # To run specify on windows : py <path\filename>
 # To run specify on MacOS : python3 <path\filename>
 print ("Hello World !!")
+
--- Python/L0002_Literals.py
+++ Python/L0002_Literals.py
@@ -1,3 +1,4 @@
+import sys
 # Strings are characters enclosed in double or single quotes
 print ("Any characters enclosed in double or single quotes is considered string in python")
 print ('I am also a string')
@@ -6,6 +7,12 @@ print ('I am also a string')
 print (5)
 print (-5)
 print (5.5)
+
+#print special number values
+print (sys.maxsize) #max int value which is power(2,63)-1
+print (sys.float_info.max) #max float value
+print (sys.float_info.min) #min float value
+print (sys.float_info.epsilon) #1.0 - least value greater than 1.0 that is representable as a difference
 
 #Print booleans which have the value True or False
 print(True)
